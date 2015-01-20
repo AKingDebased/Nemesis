e0eb6927b96e3f776fbb1aede45c4bdf12eb2bbe
AKingDebased <ggonzalv@lion.lmu.edu>
2015-01-20 01:02:38 -0800

    edited dungeon layout

---
+++ nemesis/Assets/AStarAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Pathfinding;
+
+public class AStarAI : MonoBehaviour {
+	public Vector3 targetPosition;
+	
+	void Start () {
+		Seeker seeker = GetComponent<Seeker>();
+
+		seeker.StartPath(transform.position, targetPosition, OnPathComplete);
+	}
+
+	void OnPathComplete(Path p){
+		Debug.Log ("path traced.");
+		Debug.Log ("errors: " + p.error);
+	}
+}

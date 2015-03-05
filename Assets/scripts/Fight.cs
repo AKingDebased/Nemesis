a2d7da02c7dbffb87d034098bcffd38be442aad0
gabriel <ggonzalv@lion.lmu.edu>
2015-03-05 11:50:58 -0800

    completely refactored combat code (AGAIN)
    I KNOW WHAT I AM DOING.  I AM NO LONGER LOST.

---
+++ Assets/scripts/Fight.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fight : MonoBehaviour {
+	public bool engaged = false;
+	public GameObject currentTarget;
+	
+	private Stats stats;
+	private float timeSinceLastAttack; 
+	
+	void Awake () {
+		stats = gameObject.GetComponent<Stats>();
+	}
+	
+	void Update () {
+		if(engaged){
+			if (timeSinceLastAttack * 10 >= stats.speed)
+			{
+				this.Attack(currentTarget);
+				Debug.Log ("grunt health: " + currentTarget.GetComponent<Stats>().health);
+				timeSinceLastAttack = 0;				
+			} else timeSinceLastAttack += Time.deltaTime;
+			
+			if(stats.health < 0){
+				Debug.Log ("warrior falls!");
+				Destroy(gameObject);
+			}
+		}
+	}
+	
+	private void Attack(GameObject currentTarget){
+		if (this.AttackHit(currentTarget)){
+			Debug.Log ("warrior hit!");
+			this.DoDamage(currentTarget);
+		} else {
+			Debug.Log ("warrior miss!");
+		}
+	}
+	
+	private bool AttackHit(GameObject currentTarget){
+		return Random.Range (0, 100) + (this.stats.dexterity * 2) >= (currentTarget.GetComponent<Stats>().speed);
+	}
+	
+	private void DoDamage (GameObject currentTarget){
+		if (this.stats.strength > currentTarget.GetComponent<Stats>().defense)
+		{
+			int damage = (this.stats.strength - currentTarget.GetComponent<Stats>().defense);
+			
+			if (this.IsCrit(currentTarget)) {
+				Debug.Log ("WARRIOR CRIT BABY");
+				currentTarget.GetComponent<Fight>().TakeDamage(damage * (this.stats.dexterity /10));
+			} else currentTarget.GetComponent<Fight>().TakeDamage(damage);
+		}
+		
+		else currentTarget.GetComponent<Fight>().TakeDamage(1);
+	}
+	
+	private bool IsCrit(GameObject currentTarget)
+	{
+		return ((this.stats.dexterity / 2) + Random.Range (0, 100) > currentTarget.GetComponent<Stats>().dexterity * 2);
+	}
+
+	private void TakeDamage(int damage){
+		this.stats.health -= damage;
+	}
+	
+	/*private bool InRange(GameObject currentTarget)
+	{
+		if (Vector3.Distance(currentTarget.transform.position, this.gameObject.transform.position) <= range) return true;
+		else return false;
+	}*/
+}
+
---
+++ Assets/scripts/Stats.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Stats : MonoBehaviour {
+	
+	public Dictionary<string, string> advInfo = new Dictionary<string, string>();
+	
+	public int health;
+	public int mana;
+	public int gumption;
+	public int strength;
+	public int willpower;
+	public int dexterity;
+	public int speed;
+	public int defense;
+	public int resilience;
+	
+	
+	
+	void Awake(){
+		advInfo = AdventurerGenerator.FreshHero();
+
+		if(gameObject.name == "warrior"){
+			health = 100;
+			mana = 100;
+			gumption = 100;
+			strength = 50;
+			willpower = 50;
+			dexterity = 50;
+			speed = 50;
+			defense = 20;
+			resilience = 40;
+		} 
+
+		if(gameObject.name == "grunt"){
+			health = 100;
+			mana = 100;
+			gumption = 100;
+			strength = 20;
+			willpower = 20;
+			dexterity = 20;
+			speed = 20;
+			defense = 10;
+			resilience = 20;
+		}
+	}
+	
+	
+}

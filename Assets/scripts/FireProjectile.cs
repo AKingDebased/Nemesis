394ef56277dd214ab1b8d8266b271e0dcf30debb
AKingDebased <ggonzalv@lion.lmu.edu>
2015-03-11 03:59:58 -0700

    fireballs implemented
    fireballs can be launched.  they play a cute little sound upon creation
    and impact.  still needs to be coupled with Fight code.

---
+++ Assets/scripts/FireProjectile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireProjectile : MonoBehaviour {
+	public AudioClip fireSound;
+	public GameObject projectileObj;
+	public GameObject target;
+	public float speed = 1;
+	
+	private GameObject projectile;
+	
+	void Start () {
+		projectile = Instantiate(projectileObj, transform.position, Quaternion.identity) as GameObject;
+		projectile.GetComponent<FireTowardsTarget>().target = target;
+		AudioSource.PlayClipAtPoint(fireSound,transform.position);
+	}
+}
---
+++ Assets/scripts/FireTowardsTarget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireTowardsTarget : MonoBehaviour {
+	public AudioClip collisionSound;
+	public GameObject target;
+	public GameObject collisionEffect;
+	public float speed = 18;
+
+	void Start(){
+		collisionEffect = Resources.Load("fire explosion") as GameObject;
+	}
+
+	void Update(){
+		float step = Time.deltaTime * speed;
+		
+		transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.GetInstanceID() == target.GetInstanceID()){
+			AudioSource.PlayClipAtPoint(collisionSound,transform.position);
+			Instantiate (collisionEffect, transform.position, Quaternion.identity);
+			Destroy(gameObject);
+		}
+	}
+}

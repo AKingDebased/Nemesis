69d2195ad06f9853e46e3b424306494ec61e7472
AKingDebased <ggonzalv@lion.lmu.edu>
2015-03-19 01:02:28 -0700

    manually merged patdev's changes
    cutscene and dialogue functionality now exists, though it has not been
    fully implemented

---
+++ Assets/scripts/Cutscene/Dialogue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Xml;
+
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class Dialogue : MonoBehaviour
+{
+	public string dialogueFile;
+	public Text dialogueText;
+	public Text nameText;
+
+	private XmlDocument scene;
+	private XmlNodeList dialogueList;
+	private XmlNodeList speakerList;
+
+	private int spTracker = 1;
+	private int diaTracker = 0;
+
+	private bool speakerA = true;
+ 
+	void Start()
+	{
+	     scene = new XmlDocument();
+	     scene.Load(dialogueFile);
+
+  		 speakerList = scene.GetElementsByTagName("speaker");
+
+ 		 nameText.text = speakerList[0].Attributes["character"].Value;
+ 		 dialogueText.text = speakerList[0].ChildNodes[0].InnerText;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown("space") && this.gameObject != null)
+		{
+			if (spTracker == speakerList.Count)
+			{
+				Destroy(this.gameObject);
+			}
+
+			else
+			{
+				nameText.text = speakerList[spTracker].Attributes["character"].Value;
+				dialogueText.text = speakerList[spTracker].ChildNodes[diaTracker].InnerText;
+
+				if (diaTracker == speakerList[spTracker].ChildNodes.Count - 1)
+				{
+					spTracker++;
+					diaTracker = 0;
+				}
+
+				else diaTracker++;
+			}
+		}
+	}
+
+}

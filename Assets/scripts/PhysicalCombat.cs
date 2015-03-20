e548fac8bc4a7e14c1470e8811c79c2e30d712be
AKingDebased <ggonzalv@lion.lmu.edu>
2015-03-19 18:29:45 -0700

    trying to edit commit message
    not going so well.  point is, i’ve refactored all the individual class
    combat scripts into PhysicalCombat and PhysicalFight

--- Assets/scripts/PhysicalCombat.cs
+++ Assets/scripts/PhysicalCombat.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System;
 
-public class WizardFight : MonoBehaviour {
+public class PhysicalCombat : MonoBehaviour {
 	public AudioClip attackSound;
 	
 	private Stats stats;
@@ -10,9 +10,9 @@ public class WizardFight : MonoBehaviour {
 	void Awake (){
 		stats = gameObject.GetComponent<Stats>();
 	}
-	
+
 	public void Fight(GameObject target){
-		Stats enemyStats = target.GetComponent<Stats>();
+		Stats enemyStats = target.GetComponent<Stats>(); //does this need to be assigned every time Fight is called?
 		
 		this.Attack(target); 
 		Debug.Log (target.name + " health: " + enemyStats.health);
@@ -29,33 +29,36 @@ public class WizardFight : MonoBehaviour {
 	}
 	
 	private void Attack(GameObject target){
-		gameObject.GetComponent<FireProjectile>().Fire(target);
-		//this.DoDamage(target);
+		if (this.AttackHit(target)){
+			Debug.Log (gameObject.name + " hits!");
+			AudioSource.PlayClipAtPoint(attackSound,transform.position);
+			this.DoDamage(target);
+		} else {
+			Debug.Log (gameObject.name + " misses!");
+		}
 	}
 	
-	/*private bool AttackHit(GameObject target){
+	private bool AttackHit(GameObject target){
 		return UnityEngine.Random.Range (0, 100) + (this.stats.dexterity * 2) >= (target.GetComponent<Stats>().speed);
-	}*/
+	}
 	
 	private void DoDamage (GameObject target){
-		if (this.stats.willpower > target.GetComponent<Stats>().resilience)
+		if (this.stats.strength > target.GetComponent<Stats>().defense) //we should do more than just one damage if strength is lower than defense
 		{
-			int damage = (this.stats.willpower - target.GetComponent<Stats>().resilience);
+			int damage = (this.stats.strength - target.GetComponent<Stats>().defense);
 			
-			/*if (this.IsCrit(target)) {
+			if (this.IsCrit(target)) {
 				Debug.Log (gameObject.name + " crits!");
 				target.GetComponent<Stats>().TakeDamage(damage * (this.stats.dexterity /10));
-			} else */
-
-			target.GetComponent<Stats>().TakeDamage(damage); 
+			} else target.GetComponent<Stats>().TakeDamage(damage); 
 		}
 		
 		else target.GetComponent<Stats>().TakeDamage(1);
 	}
 	
-	/*private bool IsCrit(GameObject target){
+	private bool IsCrit(GameObject target){
 		return ((this.stats.dexterity / 2) + UnityEngine.Random.Range (0, 100) > target.GetComponent<Stats>().dexterity * 2);
-	}*/
+	}
 	
 	
 }

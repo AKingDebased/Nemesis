f0e4c0e7af3448057fbf88f6f747e5c7459bec3f
Patrick Sullivan <psulli12@lion.lmu.edu>
2015-03-23 15:15:46 -0700

    dialogue change

--- Assets/scripts/Cutscene/Dialogue.cs
+++ Assets/scripts/Cutscene/Dialogue.cs
@@ -10,49 +10,44 @@ public class Dialogue : MonoBehaviour
 	public Text dialogueText;
 	public Text nameText;
 
-	private XmlDocument scene;
 	private XmlNodeList dialogueList;
 	private XmlNodeList speakerList;
 
 	private int spTracker = 1;
 	private int diaTracker = 0;
-
-	private bool speakerA = true;
  
 	void Start()
 	{
-	     scene = new XmlDocument();
-	     scene.Load(dialogueFile);
+	    XmlDocument scene = new XmlDocument();
+	    scene.Load(dialogueFile);
 
-  		 speakerList = scene.GetElementsByTagName("speaker");
+  		speakerList = scene.GetElementsByTagName("speaker");
 
- 		 nameText.text = speakerList[0].Attributes["character"].Value;
- 		 dialogueText.text = speakerList[0].ChildNodes[0].InnerText;
+ 		nameText.text = speakerList[0].Attributes["character"].Value;
+ 		dialogueText.text = speakerList[0].ChildNodes[0].InnerText;
 	}
 
 	void Update()
 	{
 		if (Input.GetKeyDown("space") && this.gameObject != null)
 		{
-			if (spTracker == speakerList.Count)
-			{
-				Destroy(this.gameObject);
-			}
-
-			else
-			{
-				nameText.text = speakerList[spTracker].Attributes["character"].Value;
-				dialogueText.text = speakerList[spTracker].ChildNodes[diaTracker].InnerText;
-
-				if (diaTracker == speakerList[spTracker].ChildNodes.Count - 1)
-				{
-					spTracker++;
-					diaTracker = 0;
-				}
-
-				else diaTracker++;
-			}
+			if (spTracker != speakerList.Count) this.NextLine();
+			else Destroy(this.gameObject);
 		}
+	}
+
+	void NextLine()
+	{
+		nameText.text = speakerList[spTracker].Attributes["character"].Value;
+		dialogueText.text = speakerList[spTracker].ChildNodes[diaTracker].InnerText;
+
+		if (diaTracker == speakerList[spTracker].ChildNodes.Count - 1)
+		{
+			spTracker++;
+			diaTracker = 0;
+		}
+
+		else diaTracker++;
 	}
 
 }

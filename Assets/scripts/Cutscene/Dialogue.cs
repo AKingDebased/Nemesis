398448b6a4a96b7c6246ac699f2353dd48d2316e
Patrick Sullivan <psulli12@lion.lmu.edu>
2015-03-25 00:13:02 -0700

    HERE YOU GO PATRICK
    may you be blessed and fruitful

--- Assets/scripts/Cutscene/Dialogue.cs
+++ Assets/scripts/Cutscene/Dialogue.cs
@@ -7,39 +7,49 @@ using UnityEngine.UI;
 public class Dialogue : MonoBehaviour
 {
 	public string dialogueFile;
+	public int sceneID;
 
 	private GameObject dialogueObject;
 	private Text dialogueText;
 	private Text nameText;
-	private XmlNodeList dialogueList;
 	private XmlNodeList speakerList;
 	private int spTracker = 1;
 	private int diaTracker = 0;
  
 	void Start()
 	{
-		XmlDocument scene = new XmlDocument();
-		scene.Load(dialogueFile);
-		speakerList = scene.GetElementsByTagName("speaker");
-
-		dialogueObject = (GameObject)Instantiate(Resources.Load("dialogue"));
-		dialogueObject.transform.SetParent(this.gameObject.transform, false);
-
-		Text[] textList = this.gameObject.GetComponentsInChildren<Text>();
-		dialogueText = textList [0];
-		nameText = textList[1];
-
- 		nameText.text = speakerList[0].Attributes["character"].Value;
- 		dialogueText.text = speakerList[0].ChildNodes[0].InnerText;
+		this.LoadXML(dialogueFile, sceneID);
+		this.CreateUIElements();
 	}
 
 	void Update()
 	{
-		if (Input.GetKeyDown("space") && this.gameObject != null)
+		if (Input.GetKeyDown("space") && dialogueObject != null)
 		{
 			if (spTracker != speakerList.Count) this.NextLine();
 			else Destroy(dialogueObject);
 		}
+	}
+
+	void LoadXML(string file, int sceneID)
+	{
+		XmlDocument sceneDoc = new XmlDocument();
+		sceneDoc.Load("Assets/scripts/Cutscene/" + file + ".xml");
+		XmlNode sceneNode = sceneDoc.GetElementsByTagName("scene")[sceneID];
+		speakerList = sceneNode.ChildNodes;
+	}
+
+	void CreateUIElements()
+	{
+		dialogueObject = (GameObject)Instantiate(Resources.Load("dialogue"));
+		dialogueObject.transform.SetParent(this.gameObject.transform, false);
+		
+		Text[] textList = this.gameObject.GetComponentsInChildren<Text>();
+		dialogueText = textList [0];
+		nameText = textList[1];
+		
+		nameText.text = speakerList[0].Attributes["character"].Value;
+		dialogueText.text = speakerList[0].ChildNodes[0].InnerText;
 	}
 
 	void NextLine()

b0cefed904797f6b02f724f3f9afaaf1c5f45da0
AKingDebased <ggonzalv@lion.lmu.edu>
2015-04-05 15:04:22 -0700

    creating main_game and title_screen scenes
    also writing a small script to load scenes on button press

---
+++ Assets/scripts/LoadSceneOnKeyPress.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadSceneOnKeyPress : MonoBehaviour {
+	
+	public bool anyKey;
+	public Object scene; //not typesafe
+
+	void Update () {
+		if(anyKey){
+			if (Input.anyKey){
+				Application.LoadLevel(scene.name);
+			}
+		}
+	}
+}

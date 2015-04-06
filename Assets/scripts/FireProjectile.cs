bba78298e05109658a6c3130a46900dd3e70eec4
AKingDebased <ggonzalv@lion.lmu.edu>
2015-04-05 21:35:06 -0700

    built main_game dungeon

--- Assets/scripts/FireProjectile.cs
+++ Assets/scripts/FireProjectile.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class FireProjectile : MonoBehaviour {
 	public AudioClip fireSound;
 	public GameObject projectileObj;
+	public Transform launchPoint;
 	public float speed = 1;
 
 	private GameObject projectile;
 	
 	public void Fire(GameObject target){
-		projectile = Instantiate(projectileObj, transform.position, Quaternion.identity) as GameObject;
+		projectile = Instantiate(projectileObj, launchPoint.position, Quaternion.identity) as GameObject;
 		projectile.GetComponent<FireTowardsTarget>().enabled = true;
 		projectile.GetComponent<FireTowardsTarget>().target = target;
 		AudioSource.PlayClipAtPoint(fireSound,transform.position);

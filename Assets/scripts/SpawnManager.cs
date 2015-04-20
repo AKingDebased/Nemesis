c7dc30dd5e694ca9ff00aea3029b27aebccb2cb3
gabriel <ggonzalv@lion.lmu.edu>
2015-04-19 19:16:25 -0700

    creating SpawnManager.cs
    minions can now be selected using number keys and spawned with a mouse
    click

---
+++ Assets/scripts/SpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+	private Ray ray;
+	private RaycastHit hit;
+	
+	public GameObject minion;
+	
+	void Update () {
+		InstantiateAtRayCast ();
+		SelectSpawnMinion ();
+	}
+	
+	private void SelectSpawnMinion(){
+		if(Input.GetKeyDown (KeyCode.Alpha1)){
+			minion = Resources.Load ("minions/grunt") as GameObject;
+		}			
+		
+		if(Input.GetKeyDown (KeyCode.Alpha2)){
+			minion = Resources.Load ("minions/shanker") as GameObject;
+		}
+		
+		if(Input.GetKeyDown (KeyCode.Alpha3)){
+			minion = Resources.Load("minions/mage") as GameObject;
+		}
+		
+		if(Input.GetKeyDown (KeyCode.Alpha4)){
+			Debug.Log ("4");
+		}
+	}
+
+	private void InstantiateAtRayCast(){
+		if (Input.GetMouseButtonDown(0)){
+			if(Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)), out hit)){
+				Debug.Log ("hit");
+				Instantiate (minion, hit.point,Quaternion.identity);
+			}
+		}
+	}
+}

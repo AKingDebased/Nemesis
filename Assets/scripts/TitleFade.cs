45af4eb59a760ce6c9adeb23689de441cae5c68f
Patrick Sullivan <psulli12@lion.lmu.edu>
2015-05-04 22:32:28 -0700

    sigh

--- Assets/scripts/TitleFade.cs
+++ Assets/scripts/TitleFade.cs
@@ -9,18 +9,26 @@ public class TitleFade : MonoBehaviour {
 	private float fadeSpeed = 0.66f;
 	private bool sceneStarting = true;
 	private bool sceneEnding = false;
+	private bool tutorialComplete = false;
+	private Dialogue tutorial;
 
 	void Awake() {
 		FadeImg.uvRect = new Rect (0, 0, Screen.width, Screen.height);
 		FadeImg.SetNativeSize ();
+		tutorial = gameObject.transform.parent.gameObject.AddComponent<Dialogue>();
 	}
  
 	void Update () {
 		if(sceneStarting)
 			StartScene();
 
-		else if(Input.GetKeyUp("space") && scene.name == "main_game")
-			sceneEnding = true;
+		else if(Input.GetKeyUp("space") && scene.name == "main_game") {
+			if (!GameObject.Find("dialogue(Clone)") && !tutorialComplete) {
+				tutorial.LoadXML("TestDialogue", 0);
+			}
+			else if (tutorialComplete)
+				sceneEnding = true;
+		}
 
 		else if (sceneEnding)
 			EndScene();

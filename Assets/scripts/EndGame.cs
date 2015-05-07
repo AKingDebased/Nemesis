bbd34921242796333a9855e052934ca79d009427
gabriel <ggonzalv@lion.lmu.edu>
2015-05-06 23:38:14 -0700

    adding end game behavior
    when any hero crosses the end trigger, text and a restart button pops up

---
+++ Assets/scripts/EndGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour {
+
+	public Text endText;
+	public Button restartButton;
+
+	void OnTriggerEnter(Collider other){
+		Instantiate(endText);
+		Instantiate(restartButton);
+	}
+}
